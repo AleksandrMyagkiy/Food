75c3877d2cc99e622143419f839f165c617f0960
Aleksandr <dentranscom@gmail.com>
2021-11-19 11:57:45 +0200

    modal

---
+++ src/js/script.js
@@ -0,0 +1,131 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+	// Tabs
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
+	// Функция которая скрывает табы
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide'); //style.display = 'none'; // Скрываем табы 
+			item.classList.remove('show', 'fade');
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active'); // Будем у каждого из табов удалять класс активности
+		});
+	}
+	// Функция которая будет показывать табы
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');  //style.display = 'block';
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	hideTabContent();
+	showTabContent(); // отображаем по умолчанию первый слайд (0)
+
+	// назначаем обработчик событий от клика  (делегирование событий)
+	tabsParent.addEventListener('click', (event) => {
+		const target = event.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+
+	// Timer
+
+	const dedline = '2021-11-23';
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60* 24)),
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector/*'.timer'*/), // selector тоисть класс .timer
+			  days = timer.querySelector('#days'),
+			  hours = timer.querySelector('#hours'),
+			  minutes = timer.querySelector('#minutes'),
+			  seconds = timer.querySelector('#seconds'),
+			  timeInterval = setInterval(updateClock, 1000);
+
+			  updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {               // если время вышло мы останавливаем счетчик
+				clearInterval(timeInterval);  
+			}
+		}
+	}
+
+	setClock('.timer', dedline);
+
+	// Modal window
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		  modal = document.querySelector('.modal'),
+		  modalCloseBtn = document.querySelector('[data-close]');
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', () => {
+			modal.classList.add('show');
+			modal.classList.remove('hide');
+			document.body.style.overflow = 'hidden';
+		});
+	});
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';
+	}
+	
+	modalCloseBtn.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === "Escape" && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+});
