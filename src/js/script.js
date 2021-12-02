ddd051f409cc29f4e90d631e2028ca8e6e8571a8
Aleksandr <dentranscom@gmail.com>
2021-12-02 08:49:36 +0200

    fetch

--- src/js/script.js
+++ src/js/script.js
@@ -238,10 +238,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			`;
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-
-			request.setRequestHeader('Content-type', 'application/json');
 			const formData = new FormData(form);
 
 			const object = {};
@@ -249,19 +245,22 @@ window.addEventListener('DOMContentLoaded', () => {
 				object[key] = value;
 			});
 
-			const json = JSON.stringify(object);
-
-			request.send(json);
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success);
-					form.reset();
-						statusMessage.remove();
-				} else {
-					showThanksModal(message.failure);
-				}
+			fetch('server.php', {
+				method: "POST",
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(object)
+			})
+			.then(data => data.text())
+			.then(data => {
+				console.log(data);
+				showThanksModal(message.success);
+				statusMessage.remove();
+			}).catch(() => {
+				showThanksModal(message.failure);
+			}).finally(() => {
+				form.reset();
 			});
 		});
 	}
@@ -289,4 +288,16 @@ window.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}, 4000);
 	}
+
+	            // Пример Fetch API
+
+	// fetch('https://jsonplaceholder.typicode.com/posts', {
+	// 	method: "POST",
+	// 	body: JSON.stringify({name: 'Alex'}),
+	// 	headers: {
+	// 		'Content-type': 'application/json'
+	// 	}
+	// })
+	// .then(response => response.json())
+	// .then(json => console.log(json));
 }); 
